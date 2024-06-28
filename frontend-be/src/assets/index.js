278600dae1c6c0ba55061c2c00ecf9f2b29d46a3
VictorMhub <victormfseveriano@hotmail.com>
2024-06-28 18:50:53 -0300

    feat(body) add responsiveness and input filter logic

--- frontend-be/src/assets/index.js
+++ frontend-be/src/assets/index.js
@@ -1,8 +1,12 @@
 import beSVG from '../assets/beSVG.svg'
 import lupeSVG from '../assets/lupeSVG.svg'
+import vector from '../assets/vector.svg'
+import ellipse from '../assets/ellipse.svg'
 
 
 export {
   beSVG,
   lupeSVG,
+  vector,
+  ellipse,
 }
